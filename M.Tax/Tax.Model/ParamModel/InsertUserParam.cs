334471ff97ed15fa6e962e27836295b62be64faf
unknown <zhengbo@zhengbo.siss.local>
2020-06-09 20:05:25 +0800

    netcore 2.升级到3.1

--- M.Tax/Tax.Model/ParamModel/InsertUserParam.cs
+++ M.Tax/Tax.Model/ParamModel/InsertUserParam.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-
-namespace Tax.Model.ParamModel
+﻿namespace Tax.Model.ParamModel
 {
-   public  class InsertUserParam:LoginParam
+    public  class InsertUserParam:LoginParam
     {
         public string Email { get; set; }
     }
